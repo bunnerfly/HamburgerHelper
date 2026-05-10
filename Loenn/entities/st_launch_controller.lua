676d644b85cfde93e73c197e1d3610b55f3f3175
bunnerfly <68405838+bunnerfly@users.noreply.github.com>
2026-05-10 12:55:57 -0600

    Add StLaunch Controller

---
+++ Loenn/entities/st_launch_controller.lua
@@ -0,0 +1,14 @@
+local controller = {}
+
+controller.name = "HamburgerHelper/StLaunchController"
+controller.justification = {0.5, 0.5}
+
+controller.placements = {
+    {
+        name = "st_launch_controller"
+    }
+}
+
+controller.texture = "loenn/hamburger/stLaunchController"
+
+return controller
